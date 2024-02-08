d4dc140ea1b9279ab6acd0cdfcecb3196acd7e9d
adityapatel-sudo <adityapatel1204@gmail.com>
2024-02-08 01:04:13 -0500

    goofy ass edittext name working

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 
 let name = document.getElementById("name")
-
+let myName = name.innerText
+let timeoutID = 0
 name.addEventListener('input', function () {
-    let cur = name.innerText
-    let aditya = 'aditya patel'
-    let count = 0;
-    for (let i = 0; i < aditya.length; i++) {
-
-    }
-    while (cur !== "aditya patel") {
-
-    }
+    startDeleteAndWrite();
 })
+
+function setCursorPosition(element, index) {
+    if (index === 0) {
+        return
+    }
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.setStart(element.firstChild, index);
+    range.collapse(true);
+    selection.removeAllRanges();
+    selection.addRange(range);
+}
+
+
+function deleteAndWrite() {
+    if (myName !== name.innerText) {
+        let count = 0
+        for (let i = 0; i < name.innerText.length; i++) {
+            if (myName.charAt(i) === name.innerText.charAt(i)) {
+                count++
+            } else {
+                break
+            }
+        }
+        if (count < name.innerText.length) {
+            name.innerText = name.innerText.slice(0, name.innerText.length - 1);
+            setCursorPosition(name, name.innerText.length)
+            timeoutID = setTimeout(deleteAndWrite, 100); // Adjust the delay here (in milliseconds)
+        } else {
+            name.innerText = myName.slice(0, name.innerText.length + 1)
+            setCursorPosition(name, name.innerText.length)
+            timeoutID = setTimeout(deleteAndWrite, 200); // Adjust the delay here (in milliseconds)
+        }
+    } else {
+        setCursorPosition(name, name.innerText.length)
+    }
+}
+
+function startDeleteAndWrite() {
+    console.log(timeoutID)
+    clearTimeout(timeoutID); // Cancel previous call if any
+    timeoutID = setTimeout(deleteAndWrite, 1000);
+}
+
