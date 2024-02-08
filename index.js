9250675a6282729b9df5bd265fae565002d1cbd3
adityapatel-sudo <adityapatel1204@gmail.com>
2024-02-08 00:18:09 -0500

    updating

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let name = document.getElementById("name")
+
+name.addEventListener('input', function () {
+    let cur = name.innerText
+    let aditya = 'aditya patel'
+    let count = 0;
+    for (let i = 0; i < aditya.length; i++) {
+
+    }
+    while (cur !== "aditya patel") {
+
+    }
+})
